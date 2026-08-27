695d1dda6f19b29d97a7b96f21e0248a7728df42
Logan Cole <lcole9@huskers.unl.edu>
2026-08-27 11:35:05 -0500

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Logan Cole <lcole9@huskers.unl.edu>
+ * Date: 8/27/2026
+ * 
+ * A simple hello world program in C, but in a git repo.
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World! My name is Logan Cole.\n");
+    
+    return 0;
+}
