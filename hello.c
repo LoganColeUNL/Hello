02be8285d531aaa4a538449245291cd5333a0f56
toasterbot37 <ryant4@huskers.unl.edu>
2026-08-27 10:12:12 -0700

    added collab info

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
 /**
  * Author: Logan Cole <lcole9@huskers.unl.edu>
  * Date: 8/27/2026
- * 
+ *
  * A simple hello world program in C, but in a git repo.
- * 
+ *
  */
 
 #include <stdio.h>
@@ -11,6 +11,8 @@
 int main(int argc, char **argv) {
     printf("Hello World! My name is Logan Cole.\n");
     printf("And my major is Computer Science!\n");
+    printf("My name is toasterbot37.\n");
+    printf("And my major is CS.\n");
 
     return 0;
 }
